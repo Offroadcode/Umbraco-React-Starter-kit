b1bf60b351de8d32329ba844456adf48d309bfb5
Pete Duncanson <pete@offroadcode.com>
2015-05-12 11:36:01 +0100

    Documenting up the Constants file

--- Orc.ReactExample/assets/JS/Constants.js
+++ Orc.ReactExample/assets/JS/Constants.js
@@ -1,12 +1,8 @@
+// keyMirror is a simple helper for making Enums (of sorts)
 var keyMirror = require('react/lib/keyMirror');
 
 module.exports = {
-    SearchTypes: keyMirror({
-        AccomOnly: null,
-        FlightOnly: null,
-        Inclusive: null
-    }),
-
+	// These characters can cause some grief if in your strings/components so we use the Unicode strings instead.
     Unicodes : {
         PoundSign : "\u00A3",
         NonBreakingSpace : "\u00A0",
@@ -14,19 +10,23 @@ module.exports = {
 		Apostrophe: "\u0027"
     },
 
+	// Always useful to have in your code, here incase you need it, does nothing right now though but at least you know where to put it!
     Debug:false,
 
+	// Flux related: Types of action that can be triggered, these are sent out to our stores via Flux
 	ActionTypes: keyMirror({
 		GOT_ARTICLES: null,
 		GETTING_FULL_ARTICLE:null,
 		FULL_DATA_RETRIEVED:null
 	}),
+
+	// Flux related: Where did our action originate from, helpful for debugging
 	PayloadSources: keyMirror({
 		SERVER_ACTION: null,
 		VIEW_ACTION: null
 	}),
 	
-	
+	// Super handy shortcuts to stop you messing up and aid readability
 	IsClientSide: typeof window !== "undefined",
 	IsServerSide: typeof window == "undefined"
 };
